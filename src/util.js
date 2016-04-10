01f5bfe4155c7ad38d4b9a555b050f2f07b8516f
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-10 15:29:01 -0700

    added missing unitless numbers
    in sync with React's CSSProperty.js

--- src/util.js
+++ src/util.js
@@ -45,6 +45,9 @@ export const recursiveMerge = (a, b) => {
  */
 var isUnitlessNumber = {
     animationIterationCount: true,
+    borderImageOutset: true,
+    borderImageSlice: true,
+    borderImageWidth: true,
     boxFlex: true,
     boxFlexGroup: true,
     boxOrdinalGroup: true,
@@ -70,8 +73,11 @@ var isUnitlessNumber = {
 
     // SVG-related properties
     fillOpacity: true,
+    floodOpacity: true,
     stopOpacity: true,
+    strokeDasharray: true,
     strokeDashoffset: true,
+    strokeMiterlimit: true,
     strokeOpacity: true,
     strokeWidth: true,
 };
