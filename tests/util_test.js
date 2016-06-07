b8e0ba8f2cf36876c8193203ce203d86b8d12798
Jordan Scales <scalesjordan@gmail.com>
2016-06-06 17:53:02 -0700

    Revert "uniquify the font-family list"
    This reverts commit cb324a1c9d17303006996624a6d8d3762a389392.

--- tests/util_test.js
+++ tests/util_test.js
@@ -1,6 +1,6 @@
 import {assert} from 'chai';
 
-import {recursiveMerge, uniquify} from '../src/util.js';
+import {recursiveMerge} from '../src/util.js';
 
 describe('Utils', () => {
     describe('recursiveMerge', () => {
@@ -25,24 +25,6 @@ describe('Utils', () => {
                     a: 1,
                     b: 2,
                 });
-        });
-    });
-
-    describe('uniquify', () => {
-        it('uniquifies an array', () => {
-            assert.deepEqual(
-                uniquify([1, 2, 3, 1]),
-                [1, 2, 3]);
-
-            assert.deepEqual(
-                uniquify(["My Font", "My Font"]),
-                ["My Font"]);
-        });
-
-        it('does not uniquify by string', () => {
-            assert.deepEqual(
-                uniquify([1, 2, "2"]),
-                [1, 2, "2"]);
         });
     });
 });
