17d971e5b01473251db81b01c6913276f58ea139
Jamie Wong <jamie.lf.wong@gmail.com>
2016-07-20 11:11:19 -0700

    Extract core of css() function into inject.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {mapObj, hashObject} from './util';
 import {
+    injectAndGetClassName,
     injectStyleOnce,
     reset, startBuffering, flushToString,
     addRenderedClassNames, getRenderedClassNames
@@ -73,20 +74,8 @@ const StyleSheetTestUtils = {
 };
 
 const css = (...styleDefinitions) => {
-    // Filter out falsy values from the input, to allow for
-    // `css(a, test && c)`
-    const validDefinitions = styleDefinitions.filter((def) => def);
-
-    // Break if there aren't any valid styles.
-    if (validDefinitions.length === 0) {
-        return "";
-    }
-
-    const className = validDefinitions.map(s => s._name).join("-o_O-");
-    injectStyleOnce(className, `.${className}`,
-        validDefinitions.map(d => d._definition));
-
-    return className;
+    // By default, append !important to all style definitions
+    return injectAndGetClassName(true, styleDefinitions);
 };
 
 export default {
