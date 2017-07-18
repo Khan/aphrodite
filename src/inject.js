263b35f98e512a8af345d02bd34d9894e0e3c655
Gil Birman <gil.birman@airbnb.com>
2017-07-17 23:19:31 -0700

    improve comment

--- src/inject.js
+++ src/inject.js
@@ -245,6 +245,7 @@ const processStyleDefinitions = (
 
 // Sum up the lengths of the stringified style definitions (which was saved as _len property)
 // and use modulus to return a single byte hash value.
+// We append this extra byte to the 32bit hash to decrease the chance of hash collisions.
 const getStyleDefinitionsLengthHash = (styleDefinitions /* : any[] */) /* : string */ => (
     styleDefinitions.reduce((length, styleDefinition) => length + styleDefinition._len, 0) % 36
 ).toString(36);
