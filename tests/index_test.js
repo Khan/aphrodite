7248751333fb19e758d3312f25029d3e46ff4662
Galen Corey <galen.corey@gmail.com>
2019-04-05 12:33:21 -0400

    revert tests

--- tests/index_test.js
+++ tests/index_test.js
@@ -143,10 +143,6 @@ describe('css', () => {
             done();
         });
     });
-
-    it("throws a useful error for invalid arguments", () => {
-        assert.throws(() => css({ color: "red" }), "Invalid Style Definition");
-    });
 });
 
 describe('StyleSheet.create', () => {
